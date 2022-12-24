805c515dd74a39cf3453ec80816c8e2fa364c18b
aminmortezaie <mohammadmortezaie@gmail.com>
2022-12-24 13:36:26 +0330

    Fix: Update.

--- decorator_example.py
+++ decorator_example.py
@@ -28,3 +28,27 @@ def devider(a,b):
 
 devider(15,5)
 devider(15,0)
+
+
+def star(func):
+    def inner(*args, **kwargs):
+        print("*" * 15)
+        func(*args, **kwargs)
+        print("*" * 15)
+    return inner
+
+
+def percent(func):
+    def inner(*args, **kwargs):
+        print("%" * 15)
+        func(*args, **kwargs)
+        print("%" * 15)
+    return inner
+
+
+@star
+@percent
+def printer(msg):
+    print(msg)
+
+printer("Hello")
